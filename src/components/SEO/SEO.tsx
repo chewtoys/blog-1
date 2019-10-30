193c0f56ff12209e705f2cb19fa70a70e15978cd
ahonn <ahonn95@outlook.com>
2019-10-30 20:31:49 +0800

    feat: update SEO configure

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -4,19 +4,14 @@ import _ from 'lodash/fp';
 
 import { getConfig } from '../../utils';
 
+const { site, social } = getConfig();
+
 interface ISEOProps {
   subTitle?: string;
   description?: string;
   canonical?: string;
   image?: string;
 }
-
-const { site, social } = getConfig();
-
-const defaultImage = {
-  url: `${site.url}${site.icon}`,
-  alt: site.title,
-};
 
 const SEO: React.SFC<ISEOProps> = (props) => {
   const { subTitle, description: desc, canonical, image } = props;
@@ -26,24 +21,26 @@ const SEO: React.SFC<ISEOProps> = (props) => {
   const url = canonical || site.siteUrl;
 
   const ogType = subTitle ? 'article' : 'website';
-  const ogImage = image ? { url: image, alt: subTitle } : defaultImage;
+  const ogImage = image && { url: image, alt: subTitle };
+
+  const openGraph = _.omitBy(_.isEmpty, {
+    url,
+    title,
+    description,
+    type: ogType,
+    images: ogImage && [ogImage],
+  });
 
   return (
     <NextSeo
       title={title}
       description={description}
       canonical={url}
-      openGraph={{
-        url,
-        title,
-        description,
-        type: ogType,
-        images: [ogImage],
-      }}
+      openGraph={openGraph}
       twitter={{
         handle: '@' + social.twitter,
         site: '@' + social.twitter,
-        cardType: 'summary_large_image',
+        cardType: 'summary',
       }}
     />
   );
